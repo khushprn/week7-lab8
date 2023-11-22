328f2ac6a2fa97a6d3e23d2e53acf44a3ca0d697
Khushpreet Singh <singhkhushp347@gmail.com>
2023-11-22 16:38:54 +0200

    week 7 lab 8 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Section from './Section';
+import Article from './Article';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
 
-function App() {
+const App = () => {
+  // Define JavaScript variables for dynamic content
+  const headerTitle = 'My React App';
+  const sectionHeading = 'Welcome to my website';
+  const sectionContent = 'This is a sample React application.';
+  const articleTitle = 'Article Title';
+  const articleText = 'This is the content of the article.';
+  const sidebarContent = 'Sidebar Content';
+  const footerText = '© 2023 My React App';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header title={headerTitle} />
+      <Section heading={sectionHeading} content={sectionContent} />
+      <Article title={articleTitle} text={articleText} />
+      <Sidebar content={sidebarContent} />
+      <Footer text={footerText} />
     </div>
   );
-}
+};
 
 export default App;
